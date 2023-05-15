e07e85901f886b0a60508c5c4b3b1eb1821cb85f
Amogh_Garg <amoghgarg9@gmail.com>
2023-05-15 16:56:02 +0530

    Information updated

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,139 +1,152 @@
 let resumeData = {
-    "name": "Amogh Garg",
-    "role": "ML Enthusiast",
-    "linkedinId":"https://www.linkedin.com/in/amogh-garg-109725213/",
-    "skypeid": "Your skypeid",
-    "roleDescription": "",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/amogh-garg-109725213/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"https://github.com/amoghgarg20",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"twitter",
-          "url":"https://twitter.com/amoghgarg9",
-          "className":"fa fa-twitter"
-        },
-        {
-          "name":"instagram",
-          "url":"https://www.instagram.com/amoghgarg20/",
-          "className":"fa fa-instagram"
-        }
-      ],
-    "aboutme":"I am a sophomore at Netaji Subas University of Technology, New Delhi, pursuing BTech in Computer Engineering. I am a Machine Learning enthusiast and also like to work on Data Analysis using Python. Also, I am proficient with working on other technologies like HTML,CSS,Javascript,React,SQL and PHP. I use C++ as a programming language for DSA.",
-    "address":"India",
-    "mobile":"8130463841",
-    "email":"amoghgarg9@gmail.com",
-    "education":[
-      {
-        "UniversityName":"Netaji Subhas University of Technology,New Delhi",
-        "specialization":"B.Tech in Computer Engineering",
-        "YearOfPassing":"2024",
-        "Marks":"9.09 CGPA upto 2nd Sem"
-      },
-      {
-        "UniversityName":"Amity International School, Mayur Vihar",
-        "specialization":"Class XII(CBSE)",
-        "YearOfPassing":"2020",
-        "Marks":"96.2%"
-      },
-      {
-        "UniversityName":"Amity International School, Mayur Vihar",
-        "specialization":"Class X(CBSE)",
-        "YearOfPassing":"2018",
-        "Marks":"96.8%"
-      }
-    ],
-    "work":[
-      {
-        "CompanyName":"International Organisation of Software Developers-NSUT",
-        "specialization":"Executive Commitee Member in ML and DSA Department",
-        "YearOfLeaving":" Since 2020",
-      },
-    ],
-    "skillsDescription":"Following are my technical skills:",
-    "skills":[
-      {
-        "skillname":"Python"
-      },
-      {
-        "skillname":"C++"
-      },
-      {
-        "skillname":"HTML"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"SQL"
-      },
-      {
-        "skillname":"PHP"
-      },
-      {
-        "skillname":"ReactJS"
-      }
-    ],
-    "sport_skills":[
-      {
-        "skillname":"Cricket"
-      },
-      {
-        "skillname":"Swimming"
-      }
-    ],
-    "portfolio":[
-      {
-        "name":"CodeInspire",
-        "description":"A site made for CodeInspire-Inspirathon to promote works of rural women.",
-        "imgurl":"images/2.png",
-        "siteurl":"https://github.com/amoghgarg20/CodeInspire"
-      },
-      {
-        "name":"DBMS Project",
-        "description":"Blood Bank Management System made using HTML,CSS,PHP AND SQL.",
-        "imgurl":"images/3.png",
-        "siteurl":"https://github.com/amoghgarg20/DBMS-Project"
-      },
-      {
-        "name":"Machine Learning",
-        "description":"Implementation of different ML concepts learnt.",
-        "imgurl":"images/4.jfif",
-        "siteurl":"https://github.com/amoghgarg20/Machine-Learning"
-      },
-      {
-        "name":"Tritinerary",
-        "description":"A site for travellers where they can plan their trip by making their own itinerary.",
-        "imgurl":"images/1.png",
-        "siteurl":"https://github.com/amoghgarg20/Tritinerary"
-      }
-    ],
-    "testimonials":[
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      },
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
-    ],
-    "album":[
-      {
-        "description":"This is a sample testimonial",
-      },
-      {
-        "description":"This is a sample testimonial",
-      }
-    ]
+  name: "Amogh Garg",
+  role: "ML Enthusiast",
+  linkedinId: "https://www.linkedin.com/in/amogh-garg/",
+  skypeid: "Your skypeid",
+  roleDescription: "",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/amogh-garg/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/amoghgarg20",
+      className: "fa fa-github",
+    },
+    {
+      name: "twitter",
+      url: "https://twitter.com/amoghgarg9",
+      className: "fa fa-twitter",
+    },
+    {
+      name: "instagram",
+      url: "https://www.instagram.com/amoghgarg20/",
+      className: "fa fa-instagram",
+    },
+  ],
+  aboutme:
+    "As a pre-final year student pursuing a degree in Computer Science and Engineering, I am eager to learn and apply my skills in the field. I have a strong foundation in programming languages such as C++ and Python, and am proficient in data structures and algorithms. I am also interested in exploring new technologies and fields such as Machine Learning and Data-Science and staying up-to-date with the latest developments in the field. I am looking for opportunities to continue learning and growing as a software engineer while making meaningful contributions to the industry",
+  address: "India",
+  mobile: "8130463841",
+  email: "amoghgarg9@gmail.com",
+  education: [
+    {
+      UniversityName: "Netaji Subhas University of Technology,New Delhi",
+      specialization: "B.Tech in Computer Engineering",
+      YearOfPassing: "2024",
+      Marks: "8.85 CGPA upto 5th Sem",
+    },
+    {
+      UniversityName: "Amity International School, Mayur Vihar",
+      specialization: "Class XII(CBSE)",
+      YearOfPassing: "2020",
+      Marks: "96.2%",
+    },
+    {
+      UniversityName: "Amity International School, Mayur Vihar",
+      specialization: "Class X(CBSE)",
+      YearOfPassing: "2018",
+      Marks: "96.8%",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "International Organisation of Software Developers-NSUT",
+      specialization: "Machine Learning Head",
+      YearOfLeaving: " Since 2022",
+    },
+    {
+      CompanyName: "International Organisation of Software Developers-NSUT",
+      specialization: "Executive Commitee Member in ML and DSA Department",
+      YearOfLeaving: " 2020-2022",
+    },
+  ],
+  skillsDescription: "Following are my technical skills:",
+  skills: [
+    {
+      skillname: "Python",
+    },
+    {
+      skillname: "C++",
+    },
+    {
+      skillname: "HTML",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "SQL",
+    },
+    {
+      skillname: "ReactJS",
+    },
+  ],
+  sport_skills: [
+    {
+      skillname: "Cricket",
+    },
+    {
+      skillname: "Swimming",
+    },
+  ],
+  portfolio: [
+    {
+      name: "VidSum",
+      description:
+        "NLP system that records online meetings/videos and generate a summary of the content in text format.",
+      imgurl: "images/2.png",
+      siteurl: "https://github.com/amoghgarg20/Summary-Generator",
+    },
+    {
+      name: "Smart Login System",
+      description:
+        "A face login system which can be easily integrated with web-applications for login purposes.",
+      imgurl: "images/2.png",
+      siteurl: "https://github.com/amoghgarg20/Login-System",
+    },
+    {
+      name: "CodeInspire",
+      description:
+        "A site made for CodeInspire-Inspirathon to promote works of rural women.",
+      imgurl: "images/2.png",
+      siteurl: "https://github.com/amoghgarg20/CodeInspire",
+    },
+    {
+      name: "DBMS Project",
+      description:
+        "Blood Bank Management System made using HTML,CSS,PHP AND SQL.",
+      imgurl: "images/3.png",
+      siteurl: "https://github.com/amoghgarg20/DBMS-Project",
+    },
+    {
+      name: "Tritinerary",
+      description:
+        "A site for travellers where they can plan their trip by making their own itinerary.",
+      imgurl: "images/1.png",
+      siteurl: "https://github.com/amoghgarg20/Tritinerary",
+    },
+  ],
+  testimonials: [
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+  ],
+  album: [
+    {
+      description: "This is a sample testimonial",
+    },
+    {
+      description: "This is a sample testimonial",
+    },
+  ],
+};
 
-  }
-  
-  export default resumeData
+export default resumeData;
